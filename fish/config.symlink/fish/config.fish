954685f33794671dcaeca12303e0e28d7a03a558
Adam Howard <skattyadz+gh@gmail.com>
2014-09-03 23:49:46 +0100

    Add a bunch of stuff

---
+++ fish/config.symlink/fish/config.fish
@@ -0,0 +1,16 @@
+set fish_color_normal c5c8c6
+set fish_color_command 81a2be
+set fish_color_quote de935f
+set fish_color_redirection b294bb
+set fish_color_end b294bb
+set fish_color_error cc6666
+set fish_color_param 81a2be
+set fish_color_comment 969896
+set fish_color_match 8abeb7
+set fish_color_search_match b294bb
+set fish_color_operator 8abeb7
+set fish_color_escape 8abeb7
+set fish_color_cwd b5bd68
+
+
+set -g -x PATH /usr/local/bin $PATH
