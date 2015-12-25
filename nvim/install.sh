969dfcc0c9e682f937fbbbee3fc13651e70c1a38
Adam Howard <skattyadz+gh@github.com>
2015-12-25 11:58:39 +0000

    Use .config for both fish and neovim

---
+++ nvim/install.sh
@@ -0,0 +1,7 @@
+if [ ! -f ~/.config/nvim/autoload/plug.vim ]
+then
+  curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# nvim +PlugInstall +qall
