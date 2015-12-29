66138edeca788b4364176dc6c62f0bd19c9c4ff4
Adam Howard <skattyadz+gh@gmail.com>
2015-12-29 01:17:24 +0100

    Remove set_active_vim_file_env until I have a non-tmux version (or a better solution)

--- fish/config/fish.symlink/config.fish
+++ fish/config/fish.symlink/config.fish
@@ -12,12 +12,12 @@ set fish_greeting '' # turn off greeting
 
 # my vim config prints an ACTIVE_VIM_BUFFFER_PATH session env. We want this to
 # be accessible through $V
-function -e fish_preexec set_active_vim_file_env
-  set -l BUFFER_PATH (tmux show-environment ACTIVE_VIM_BUFFER_PATH ^/dev/null)
-  if test $BUFFER_PATH
-    set -g V (echo $BUFFER_PATH | sed "s:^.*=::")
-  end
-end
+# function -e fish_preexec set_active_vim_file_env
+#   set -l BUFFER_PATH (tmux show-environment ACTIVE_VIM_BUFFER_PATH ^/dev/null)
+#   if test $BUFFER_PATH
+#     set -g V (echo $BUFFER_PATH | sed "s:^.*=::")
+#   end
+# end
 
 set -u fish_user_paths $fish_user_paths /usr/local/var/rbenv/shims
 set -gx RBENV_ROOT /usr/local/var/rbenv
