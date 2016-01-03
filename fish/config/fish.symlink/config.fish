5c658495326653f709ab818c16efe0c21bc95d75
Adam Howard <skattyadz+gh@gmail.com>
2016-01-03 00:35:34 +0000

    Move fish functions to files

--- fish/config/fish.symlink/config.fish
+++ fish/config/fish.symlink/config.fish
@@ -1,35 +1,5 @@
 if status --is-interactive
-  eval sh $HOME/.config/base16-shell/base16-ocean.dark.sh
-
-  set BASE_03 65737e
-
-  # will need to tweak this over time. not sure about the contrast between command, param, and quote atm
-  set -g fish_color_autosuggestion    $BASE_03
-  set -g fish_color_command           normal
-  set -g fish_color_comment           $BASE_03
-  set -g fish_color_cwd               green
-  set -g fish_color_cwd_root          red
-  set -g fish_color_end               purple
-  set -g fish_color_error             red
-  set -g fish_color_escape            cyan
-  set -g fish_color_history_current   cyan
-  set -g fish_color_host              cyan
-  set -g fish_color_match             cyan
-  set -g fish_color_normal            normal
-  set -g fish_color_operator          cyan
-  set -g fish_color_param             blue
-  set -g fish_color_quote             cyan
-  set -g fish_color_redirection       cyan
-  set -g fish_color_search_match      \x2d\x2dbackground\x3dpurple
-  set -g fish_color_selection         \x2d\x2dbackground\x3dpurple
-  set -g fish_color_status            red
-  set -g fish_color_user              \x2do\x1egreen
-  set -g fish_color_valid_path        \x2d\x2dunderline
-  set -g fish_pager_color_completion  normal
-  set -g fish_pager_color_description $BASE_03
-  set -g fish_pager_color_prefix      cyan
-  set -g fish_pager_color_progress    cyan
-
+ set_colors
 end
 
 set -x PATH /usr/local/bin $PATH # homebrew
@@ -50,20 +20,6 @@ set fish_greeting '' # turn off greeting
 #     set -g V (echo $BUFFER_PATH | sed "s:^.*=::")
 #   end
 # end
-
-function set_abbreviations
-  abbr -a ga   git add
-  abbr -a gap  git add -p
-  abbr -a gc   git commit
-  abbr -a gcm  git commit -m
-  abbr -a gco  git checkout
-  abbr -a gs   git status -s
-  abbr -a gb   git branch
-  abbr -a gd   git diff
-
-  abbr -a .. cd ..
-  abbr -a ll ls -ll
-end
 
 set_abbreviations
 
---
+++ fish/config/fish.symlink/functions/set_abbreviations.fish
@@ -0,0 +1,13 @@
+function set_abbreviations
+  abbr -a ga   git add
+  abbr -a gap  git add -p
+  abbr -a gc   git commit
+  abbr -a gcm  git commit -m
+  abbr -a gco  git checkout
+  abbr -a gs   git status -s
+  abbr -a gb   git branch
+  abbr -a gd   git diff
+
+  abbr -a .. cd ..
+  abbr -a ll ls -ll
+end
