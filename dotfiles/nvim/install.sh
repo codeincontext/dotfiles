fca94ec14d50b4da874bb53f42b3b8c34db4b57a
Adam Howard <skattyadz+gh@gmail.com>
2017-09-27 14:20:18 +0100

    Tidy

--- dotfiles/nvim/install.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -f ~/.config/nvim/autoload/plug.vim ]
-then
-  curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-# nvim +PlugInstall +qall
