afabafb9db241997feae25c936868171e4a65923
Adam Howard <skattyadz+gh@gmail.com>
2019-10-07 13:44:27 +0100

    WIP

--- dotfiles-scripts/node/install.sh
+++ dotfiles-scripts/node/install.sh
@@ -1,10 +1,3 @@
 n latest
-npm install -g nodemon
 npm install -g eslint
 npm install -g react-native-cli
-npm install -g bower
-npm install -g create-react-app
-npm install -g standard-format
-npm install -g tern
-npm install -g js-beautify
-npm install -g stylelint
